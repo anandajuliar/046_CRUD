6a9e005d19e082998df12ee46780529470c3e1d1
anandajuliar <anandajulia001@gmail.com>
2025-10-20 12:26:37 +0700

    connect to db

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require ("express");
+let mysql = require("mysql2");
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+app.use(express.urlencoded({extended:true})),
+
+app.get('/', (req,res) => {
+    res.send("hello world");
+})
+
+app.listen(PORT, () => {
+    console.log('server is running on port${PORT}');
+})
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Akusukses15!',
+    database: 'biodata',
+    port: 3306
+})
